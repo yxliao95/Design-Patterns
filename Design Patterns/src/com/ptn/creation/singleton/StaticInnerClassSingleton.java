a3e0eb8b141818dca96614cd35a2c9b5091d7b37
liao <727687719@qq.com>
2019-03-26 19:46:31 +0800

    improve StaticInnerClassSingleton

--- Design Patterns/src/com/ptn/creation/singleton/StaticInnerClassSingleton.java
+++ Design Patterns/src/com/ptn/creation/singleton/StaticInnerClassSingleton.java
@@ -2,16 +2,11 @@ package com.ptn.creation.singleton;
 
 public class StaticInnerClassSingleton {
 
-	private StaticInnerClassSingleton singleton;
-
 	private static class InnerClass {
 		static StaticInnerClassSingleton singleton = new StaticInnerClassSingleton();
 	}
 
 	public StaticInnerClassSingleton getInstance() {
-		if (singleton == null) {
-			singleton = InnerClass.singleton;
-		}
-		return singleton;
+		return InnerClass.singleton;
 	}
 }
